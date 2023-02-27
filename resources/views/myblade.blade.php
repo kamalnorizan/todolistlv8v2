1ae424d51fdc9d45f2d06d12aa9632a8713f99c1
Ahmad Kamal B Norizan <kamalnorizan@gmail.com>
2023-02-27 15:53:58 +0800

    init

---
+++ resources/views/myblade.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    <a href="{{route('myblade123',['name'=>'Saiful'])}}">Link to My Blade 2</a> <br>
+    Hello World <br>
+    {{ 'Hello using echo' }}
+</body>
+</html>
---
+++ routes/web.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UserController;
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+DB::listen(function ($event) {
+    dump($event->sql);
+});
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/myblade', function () {
+    return view('myblade');
+});
+
+Route::get('/myblade2/{name?}', function ($name=null) {
+    return view('myblade2', compact('name'));
+})->name('myblade123');
+
+Route::resource('user', UserController::class);
+
