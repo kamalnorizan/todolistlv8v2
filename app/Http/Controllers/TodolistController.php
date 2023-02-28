58e2c76d647e475dd5095e434f37fa9682b9af64
Ahmad Kamal B Norizan <kamalnorizan@gmail.com>
2023-02-28 15:42:15 +0800

    migration

---
+++ app/Http/Controllers/TodolistController.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Todolist;
+use App\Http\Requests\StoreTodolistRequest;
+use App\Http\Requests\UpdateTodolistRequest;
+
+class TodolistController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \App\Http\Requests\StoreTodolistRequest  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(StoreTodolistRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Todolist  $todolist
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Todolist $todolist)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Todolist  $todolist
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Todolist $todolist)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \App\Http\Requests\UpdateTodolistRequest  $request
+     * @param  \App\Models\Todolist  $todolist
+     * @return \Illuminate\Http\Response
+     */
+    public function update(UpdateTodolistRequest $request, Todolist $todolist)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Todolist  $todolist
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Todolist $todolist)
+    {
+        //
+    }
+}
---
+++ database/migrations/2023_02_28_072344_create_comments_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCommentsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('comments', function (Blueprint $table) {
+            $table->id();
+            $table->text('comment');
+            $table->bigInteger('todolist_id')->unsigned();
+            $table->bigInteger('user_id')->unsigned();
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+            $table->foreign('todolist_id')->references('id')->on('todolists')->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('comments');
+    }
+}
