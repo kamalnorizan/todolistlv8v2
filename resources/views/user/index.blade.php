29cfa8961d81d6dcebdcd778594d9650c4a1d143
Ahmad Kamal B Norizan <kamalnorizan@gmail.com>
2023-02-28 12:17:14 +0800

    popup modal

--- resources/views/user/index.blade.php
+++ resources/views/user/index.blade.php
@@ -12,6 +12,10 @@
                 <div class="card-header">
                     Senarai User
                     <a href="{{route('user.create')}}" class="btn btn-primary float-right">Create User Page</a>
+
+                    <button type="button" class="btn btn-warning float-right " data-toggle="modal" data-target="#createUser-mdl">
+                        Create User Popup
+                      </button>
                 </div>
 
                 <div class="card-body">
@@ -51,6 +55,26 @@
                         </tfoot>
                     </table>
                 </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+<div class="modal fade" id="createUser-mdl" tabindex="-1" role="dialog" aria-labelledby="modelTitleId" aria-hidden="true">
+    <div class="modal-dialog" role="document">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title">Create New User</h5>
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
+                    </button>
+            </div>
+            <div class="modal-body">
+                @include('user._form')
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                <button type="button" class="btn btn-primary">Save</button>
             </div>
         </div>
     </div>
